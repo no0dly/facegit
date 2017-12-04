cc2431ba665d19089fed8b8ee54f04f1f4e23808
Vadim Derman <thenoodly@gmail.com>
2017-12-03 22:42:51 -0800

    saga auth tests part 2

--- src/sagas/__tests__/auth.test.js
+++ src/sagas/__tests__/auth.test.js
@@ -1,6 +1,6 @@
 import { authFlow } from "../auth";
 import { authorize, logout } from "../../actions/auth";
-import { select, call, take } from "redux-saga/effects";
+import { select, call, take, put } from "redux-saga/effects";
 import { getIsAuthorized } from "../../reducers/auth";
 import {
   getTokenFromLocalStorage,
@@ -48,9 +48,35 @@ describe("Сага authFlow", () => {
   });
 
   describe("Сценарий c токеном авторизации из localstorage", () => {
-    it("Is test present?", () => {
-      const isTestsPresent = false;
-      expect(isTestsPresent).toEqual(false);
+    it("1. Select getIsAuthorized", () => {
+      expect(saga.next().value).toEqual(select(getIsAuthorized));
+    });
+    it("2. Should call getTokenFromLocalStorage", () => {
+      expect(saga.next().value).toEqual(call(getTokenFromLocalStorage));
+    });
+    it("3. Should dispatch athorize action", () => {
+      expect(saga.next(token).value).toEqual(put(authorize(token)));
+    });
+    it("4. Should call(setTokenApi, token) with token value, from previous step", () => {
+      expect(saga.next({ payload: token }).value).toEqual(
+        call(setTokenApi, token)
+      );
+    });
+
+    it("5. Call setTokenToLocalStorage", () => {
+      expect(saga.next().value).toEqual(call(setTokenToLocalStorage, token));
+    });
+
+    it("6. Listen take logout", () => {
+      expect(saga.next().value).toEqual(take(logout));
+    });
+
+    it("7. Call removeTokenFromLocalStorage", () => {
+      expect(saga.next().value).toEqual(call(removeTokenFromLocalStorage));
+    });
+
+    it("8. Call clearTokenApi", () => {
+      expect(saga.next().value).toEqual(call(clearTokenApi));
     });
   });
 });
