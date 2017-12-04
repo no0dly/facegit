9d7843276ba923b3b772037872a7d48525b6910e
Vadim Derman <thenoodly@gmail.com>
2017-12-04 13:58:32 -0800

    test

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ zip -r build.zip build
 
 curl -H "Content-type: application/zip" \
      -H "Authorization: Bearer $NETLIFY_TOKEN" \
-     -d "{\"quiet\": true}" \
      --data-binary "@build.zip" \
      https://api.netlify.com/api/v1/sites
