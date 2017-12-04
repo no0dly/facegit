965caa688748b26f935391d791cc8004fdeaf99f
Vadim Derman <thenoodly@gmail.com>
2017-12-04 13:51:25 -0800

    deplay fix 2

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ zip -r build.zip build
 
 curl -H "Content-type: application/zip" \
      -H "Authorization: Bearer $NETLIFY_TOKEN" \
-     --data-binary "@build.zip" \
-     https://api.netlify.com/api/v1/sites \
      -d "{\"quiet\": true}" \
+     --data-binary "@build.zip" \
+     https://api.netlify.com/api/v1/sites
